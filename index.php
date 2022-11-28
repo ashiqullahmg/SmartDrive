78934c30ff85a3ca7e678fc71eed4f074263ed30
Ashiq Ullah <43939523+ashiqullahmg@users.noreply.github.com>
2022-11-28 01:50:00 -0800

    Update index.php
    API Key is removed.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    <title>Smart Drive</title>
    <!--Google API-->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-   <script src="https://maps.google.com/maps/api/js?key=AIzaSyBjku55AW8rs4Nr_Re4UUU4YAeEmnef-rA"></script>
+   <script src="https://maps.google.com/maps/api/js?key=YOU-MAY-USE-UR-OWN-API-KEY"></script>
    <!--Boostrap-->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
